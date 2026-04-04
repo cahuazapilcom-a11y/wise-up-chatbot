592da731a9bc954117d76963ce1c56fafb4422dc
moises cahuaza <cahuazapilcom@gmail.com>
2026-04-04 09:15:56 -0500

    debug tipos de routers

--- index.js
+++ index.js
@@ -87,5 +87,3 @@ app.listen(PORT, () => {
 process.on('unhandledRejection', (err) => console.error('❌ unhandledRejection:', err));
 process.on('uncaughtException',  (err) => console.error('❌ uncaughtException:',  err));
 
-app.use('/webhook', webhookRouter);
-// app.use('/crm', crmRouter);
